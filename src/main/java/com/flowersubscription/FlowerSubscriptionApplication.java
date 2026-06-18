f41e1c8b3b3e28059b8e884657e9222cdf8577ef
Valentyn_Koval <J.K.play2015@gmail.com>
2026-06-18 20:33:36 +0300

    initial commit

---
+++ src/main/java/com/flowersubscription/FlowerSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.flowersubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerSubscriptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowerSubscriptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flowersubscription/FlowerSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowersubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerSubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
